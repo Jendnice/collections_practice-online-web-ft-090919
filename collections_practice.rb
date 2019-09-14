d8ce0ed29882ed949bbc196d9504b2efcf5c3738
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-14 00:03:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -35,7 +35,7 @@ def sum_array(array)
 end 
 
 def add_s(array)
-  new_array = []
+  while array 
   
   new_array << array.each_with_index.collect do |word| puts "#{word}s" unless word == array[1] 
   end
